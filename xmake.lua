14beb7aec08095f1a1ac710f78132c15962ae8ab
ShrBox <shrbox@outlook.com>
2026-09-07 10:50:50 +0800

    chore: prepare CHANGELOG for v26.32.2

--- xmake.lua
+++ xmake.lua
@@ -51,11 +51,11 @@ end
 
 if is_server then
     if is_windows then
-        add_requires("bedrockdata v26.32.2-server.4")
+        add_requires("bedrockdata v26.32.2-server.6")
     end
 else
     if is_windows then
-        add_requires("bedrockdata v26.32.2-client.4")
+        add_requires("bedrockdata v26.32.2-client.6")
     end
 end
 
